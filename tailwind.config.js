e50f928f1af8dec14689c0db35f7b5348c3732c7
abushamacd <abushamacd@gmail.com>
2024-08-24 11:48:44 +0600

    start frontend

---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+    colors: {
+      primary: "#0b2952",
+      secondary: "#1f4e8d",
+      track_bg: "#4a88da",
+      bg: "#f9fafb",
+      white: "#ffffff",
+    },
+    container: {
+      center: true,
+    },
+    screens: {
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 992px) { ... }
+
+      xl: "1200px",
+      // => @media (min-width: 1200px) { ... }
+
+      "2xl": "1500px",
+      // => @media (min-width: 1400px) { ... }
+    },
+  },
+  plugins: [],
+};
