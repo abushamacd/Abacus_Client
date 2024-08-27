3f1c639b3f810e0a47e803aef23d7abcb39bd29c
abushamacd <abushamacd@gmail.com>
2024-08-27 16:50:19 +0600

    add theme toggle switch

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,8 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { RouterProvider } from "react-router-dom";
-import { useEffect, useState } from "react";
+
 import routes from "./utils/routes";
 
 function App() {
-  const [theme, setTheme] = useState("dark");
-  useEffect(() => {
-    if (theme === "dark") {
-      document.documentElement.classList.add("dark");
-    } else {
-      document.documentElement.classList.remove("dark");
-    }
-  }, [theme]);
-
-  const handleTheme = () => {
-    setTheme(theme === "dark" ? "light" : "dark");
-    console.log("tiger");
-  };
-
   return (
     <>
       <RouterProvider router={routes} />
--- src/layouts/AdminLayout.tsx
+++ src/layouts/AdminLayout.tsx
@@ -1,16 +1,32 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
-import { useState } from "react";
-import { Breadcrumb, Layout, Menu, theme } from "antd";
+import { useEffect, useState } from "react";
+import { Breadcrumb, Layout, Menu, Switch, theme } from "antd";
 import { sidebarItems } from "../constants/sidebarItems";
 import { getUserInfo } from "../services/auth.service";
+import { CheckOutlined, CloseOutlined } from "@ant-design/icons";
+import { getFromLocalStorage, setToLocalStorage } from "../utils/local-storage";
 
 const { Header, Content, Sider } = Layout;
 
 export const AdminLayout = () => {
   const [collapsed, setCollapsed] = useState(false);
+
   const {
     token: { colorBgContainer, borderRadiusLG },
   } = theme.useToken();
+
+  const themeMood = getFromLocalStorage("theme");
+
+  const [globalTheme, setGlobalTheme] = useState(
+    (themeMood === "light" ? true : false) || true
+  );
+
+  useEffect(() => {
+    document.documentElement.classList.toggle("dark");
+    setToLocalStorage("theme", globalTheme ? "dark" : "light");
+  }, [globalTheme]);
+
+  console.log(themeMood);
 
   // @ts-ignore
   const { role } = getUserInfo();
@@ -31,9 +47,19 @@ export const AdminLayout = () => {
         />
       </Sider>
       <Layout>
-        <Header style={{ padding: 0, background: colorBgContainer }} />
+        <Header style={{ padding: 0, background: colorBgContainer }}>
+          <Switch
+            onChange={(checked) => setGlobalTheme(checked)}
+            checkedChildren={<CheckOutlined />}
+            unCheckedChildren={<CloseOutlined />}
+            checked={globalTheme}
+          />
+        </Header>
         <Content style={{ margin: "0 16px" }}>
-          <Breadcrumb style={{ margin: "16px 0" }}>
+          <Breadcrumb
+            className="text-primary dark:text-mirage"
+            style={{ margin: "16px 0" }}
+          >
             <Breadcrumb.Item>User</Breadcrumb.Item>
             <Breadcrumb.Item>Bill</Breadcrumb.Item>
           </Breadcrumb>
